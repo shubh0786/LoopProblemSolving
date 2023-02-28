756abe6b8601bc6b67735178a81ba1661851714a
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-28 13:10:17 +1300

    Add project files.

---
+++ LoopPro1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program in C# Sharp to display the first 10 natural numbers.
+
+namespace LoopPro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int t;
+            
+
+            Console.WriteLine("Following are the first natural");
+
+
+            for(i=10; i <100; i++)
+            {
+
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
