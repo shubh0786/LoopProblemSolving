2d602036357f6f768c5d6fbb70b5325cdfc537a7
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-01 01:41:48 +1300

    Problem 2

---
+++ loopPro2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Write a C# Sharp program to find the sum of first 10 natural numbers. 
+
+namespace loopPro2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int i, sum = 0;
+
+            for ( i = 1; i <=10; i++)
+            {
+
+                sum = sum + i;
+                Console.WriteLine("{0}",i);
+
+
+     
+            }
+            Console.WriteLine("\n The Sum is : {0}\n", sum);
+
+            Console.ReadLine();
+        }
+
+    }
+}
