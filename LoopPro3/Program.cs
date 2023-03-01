3a7975299c9e5f7afb81b1348e9515bf2d8dd05d
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-01 23:56:53 +1300

    problem defination

--- LoopPro3/Program.cs
+++ LoopPro3/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Write a program in C# Sharp to display n terms of natural number and their sum
+
 namespace LoopPro3
 {
     internal class Program
