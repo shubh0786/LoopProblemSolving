a29b03226501534e118939e1c85ccc6098276940
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-03-20 23:51:17 +1300

    Problem Added 5

---
+++ LoopPro5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopPro5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            motivate();
+
+            Console.WriteLine(  "\n");
+
+            Faith();
+
+            Console.ReadLine();
+
+        }
+
+        static void motivate() 
+        
+        
+        
+        {
+
+
+            Console.WriteLine("I will do it I shoud do it ");
+
+        }
+
+        static void Faith()
+        {
+            Console.WriteLine("Lets finish once for all");
+                
+                }
+    }
+}
